15f51f879f657701a3165483e8fe5a655aba0483
Noah Luna <15202580+ngrayluna@users.noreply.github.com>
2026-02-25 18:50:28 -0800

    check for top level group command

--- sort_markdown.py
+++ sort_markdown.py
@@ -1,8 +1,12 @@
 """
 Check if a Click command belongs to a Group. If it does, create a directory
-for the group and place the commands inside it. If not, place the command in
-the root directory. This helps organize the documentation by grouping related
-commands together.
+for the group and place the commands inside it.
+
+Do not move the top level Group commands, only the subcommands. For example,
+if we have a group "docker" with subcommands "run" and "build", create a
+directory "wandb-docker" and move "wandb-docker-run.md" and
+"wandb-docker-build.md" into "wandb-docker". The "wandb-docker.md" file 
+should remain in the top level output directory.
 
 Python commands that are Click groups have "is_click_group": true in the
 output of get_public_commands_with_source(). We can use this to determine
@@ -39,6 +43,7 @@ if __name__ == "__main__":
     with open(args.source_info, "w") as f:
         json.dump(public_commands, f, indent=4)
 
+    # Get list of click groups from the commands
     click_group = []
     for cmd in public_commands:
         if cmd['is_click_group']:
@@ -47,7 +52,6 @@ if __name__ == "__main__":
     # Make a directory for each click group
     for group in click_group:
         group_dir = os.path.join(args.output_markdown, "wandb-" + group['name'])
-        #print(f"Creating directory for group {group['name']}: {group_dir}")
         if not os.path.exists(group_dir):
             os.makedirs(group_dir)
 
@@ -57,12 +61,9 @@ if __name__ == "__main__":
     for filename in os.listdir(args.output_markdown):
         if not filename.endswith(".md"):
             continue
-        #print(f"Checking file: {filename}")
         for group in click_group:
-            if group['name'] in filename:
+            if group['name'] in filename and filename != f"{group['name']}.md":
                 old_path = os.path.join(args.output_markdown, filename)
-                #print(f"Moving {old_path} to {group['name']} directory")
 
                 new_path = os.path.join(args.output_markdown, "wandb-" + group['name'], filename)
-                # print(f"New path: {new_path}")
                 os.rename(old_path, new_path)
